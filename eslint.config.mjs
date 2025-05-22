5cb34bffd34a911aed0a4c1dd469627f1eb5a4f7
Henrique Garcia <henriquegarcia.dev@gmail.com>
2025-05-22 08:28:03 -0300

    feat: Setup inicial do projeto

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      'react/jsx-key': 'error',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@next/next/no-html-link-for-pages': 'off',
+    },
+  },
+];
+
+export default eslintConfig;
